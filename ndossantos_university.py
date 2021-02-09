d93fa1678b24ba86ec301c1442cd333fad901b45
Electronick79 <ndossantos@student.brigew.edu>
2021-02-09 09:52:20 -0500

    get the following fields at least:
    school.name
    
    school.city
    
    2018.student.size
    
    2017.student.size
    
    2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line
    
    2016.repayment.3_yr_repayment.overa

--- ndossantos_university.py
+++ ndossantos_university.py
@@ -1,12 +1,30 @@
-def square_each(nums):
+import secrets
+import requests
+
+
+def get_data(url:str):
+    all_data = []
+    full_url = f"{url} & api_key = {secrets.api_key()}"
+    for page in range(10):
+        response = requests.get(C:\Users\Electronick\OneDrive\Desktop\COMP_490\project1):
+        if response.status_code != 300:
+            print("error getting data")
+            exit(-1)
+        page_of_data = response.json()
+        page_of_school_data = page_of_school_data['result']
+        all_data.extend(page_of_school_data)
+    return all_data
+
+def city_each(nums):
     for i in range(len(nums)):
         nums[i] = nums[i] ** 2
 
 
-def sum_list(nums):
+
+def sum_list(school_nums):
     total=0
-    for i in range(len(nums)):
-        total+=nums[i]
+    for i in range(len(school_nums)):
+        total+=school_nums[i]
     return total
 
 def to_numbers(str_list):
@@ -20,9 +38,9 @@ def to_numbers(str_list):
 
 
 def main():
-    filename = input('Please enter the file name: ')
+    filename = input('Please enter the school name: ')
   str_list=[]
-    with open(filename,'r') as infile:
+    with open(schoolname,'r') as infile:
         for line in infile.readlines():
             if ',' in line:
                 line = line.strip().split(',')
@@ -31,8 +49,8 @@ def main():
             for val in line: str_list.append(val.strip())
 
     to_numbers(str_list)
-    square_each(str_list)
+    city_each(str_list)
     total = sum_list(str_list)
-    print('The sum of the squares of the numbers in the file is {}'.format(total))
+    print('school.name, school.city,2018.student.size, 2017.student.size, 2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line2016.repayment.3_yr_repayment.overal {}'.format(total))
 
 main()
